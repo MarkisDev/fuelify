f493cf5bfdf888c343d865a5ffe287612d2e1cee
Rijuth Menon <rijuthm@gmail.com>
2023-01-08 20:44:16 +0530

    added fuel routes to app

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, session, redirect, url_for, render_template
-from routes.fuel_inventory import fuel_routes
+from routes.fuel_inventory import fuel_inventory_routes
+from routes.fuel_purchases import fuel_purchases_routes
 from routes.auth import auth_routes
 from routes.customer import customer_routes
 from routes.employee import employee_routes
@@ -7,10 +8,11 @@ from routes.employee import employee_routes
 app = Flask(__name__)
 app.secret_key = 'secretKey'
 
-app.register_blueprint(fuel_routes)
+app.register_blueprint(fuel_inventory_routes)
 app.register_blueprint(auth_routes)
 app.register_blueprint(customer_routes)
 app.register_blueprint(employee_routes)
+app.register_blueprint(fuel_purchases_routes)
 
 
 @app.route('/')
