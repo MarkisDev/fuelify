04ce4fb025f126f4fad10593669fb07e948c8572
Rijuth Menon <rijuthm@gmail.com>
2023-01-08 17:35:23 +0530

    registered employee routes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, session, redirect, url_for, render_template
 from routes.fuel_inventory import fuel_routes
 from routes.auth import auth_routes
 from routes.customer import customer_routes
+from routes.employee import employee_routes
 
 app = Flask(__name__)
 app.secret_key = 'secretKey'
@@ -9,6 +10,7 @@ app.secret_key = 'secretKey'
 app.register_blueprint(fuel_routes)
 app.register_blueprint(auth_routes)
 app.register_blueprint(customer_routes)
+app.register_blueprint(employee_routes)
 
 
 @app.route('/')
