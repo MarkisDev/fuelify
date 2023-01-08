8a6de5c13ec151e6ac77e147cfa569fac1770946
Rijuth Menon <rijuthm@gmail.com>
2023-01-08 11:51:32 +0530

    made a helper function for login

---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+from functools import wraps
+from flask import session, redirect, url_for, flash
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'user_id' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Please login first!')
+            return redirect(url_for('login'))
+    return wrap
