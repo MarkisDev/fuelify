319b1c6a6a4ed6fb36eb73053a6ba27e5ddedcd6
Rijuth Menon <rijuthm@gmail.com>
2023-01-08 11:51:03 +0530

    created auth routes

---
+++ routes/auth.py
@@ -0,0 +1,39 @@
+from models.user import User
+from utils.utils import login_required
+
+from flask import Blueprint, request, redirect, url_for, flash, render_template, session
+
+auth_routes = Blueprint('auth_routes', __name__)
+
+
+@auth_routes.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user_model = User()
+        result = user_model.validate_login(username, password)
+        if result:
+            session['user_id'] = result[0]
+            session['username'] = result[1]
+            session['role'] = result[4]
+            return redirect(url_for('auth_routes.dashboard'))
+        else:
+            flash('Incorrect username or password')
+    return render_template('login.html')
+
+
+@auth_routes.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    session.pop('username', None)
+    session.pop('role', None)
+    return redirect(url_for('auth_routes.login'))
+
+
+@auth_routes.route('/dashboard')
+@login_required
+def dashboard():
+    user_id = session['user_id']
+    user_data = User().get_user_data(user_id)
+    return render_template('dashboard.html', user=user_data)
