f9333b16606850201fedab5ea35a0d71bfc9d4dd
Rijuth Menon <rijuthm@gmail.com>
2023-01-08 20:43:39 +0530

    added fuel_purchases

--- models/fuel_purchases.py
+++ models/fuel_purchases.py
@@ -13,14 +13,48 @@ class FuelPurchases(Database):
             (employee_id, customer_id, fuel_id, quantity, price, purchase_date)
         )
 
-    def get_all(self):
-        cursor = self.execute(
+    def update(self, purchase_id, employee_id, customer_id, fuel_id, quantity, price, purchase_date):
+        self.execute(
             """
-            SELECT p.purchase_id, e.first_name, e.last_name, c.first_name, c.last_name, f.fuel_type, p.quantity, p.price, p.purchase_date
-            FROM fuel_purchases p
-            JOIN employees e ON p.employee_id = e.employee_id
-            JOIN customer_information c ON p.customer_id = c.customer_id
-            JOIN fuel_inventory f ON p.fuel_id = f.fuel_id
-            """
+            UPDATE fuel_purchases
+            SET employee_id=?, customer_id=?, fuel_id=?, quantity=?, price=?, purchase_date=?
+            WHERE purchase_id=?
+            """,
+            (employee_id, customer_id, fuel_id,
+             quantity, price, purchase_date, purchase_id)
         )
+
+    def delete(self, purchase_id):
+        self.execute(
+            """
+            DELETE FROM fuel_purchases
+            WHERE purchase_id=?
+            """,
+            (purchase_id,)
+        )
+
+    def get(self, purchase_id=None):
+        if purchase_id:
+            cursor = self.execute(
+                """
+                SELECT p.purchase_id, e.first_name, e.last_name, c.first_name, c.last_name, f.fuel_type, p.quantity, p.price, p.purchase_date
+                FROM fuel_purchases p
+                JOIN employees e ON p.employee_id = e.employee_id
+                JOIN customer_information c ON p.customer_id = c.customer_id
+                JOIN fuel_inventory f ON p.fuel_id = f.fuel_id
+                WHERE p.purchase_id = ?
+                """,
+                (purchase_id,)
+            )
+            return cursor.fetchone()
+        else:
+            cursor = self.execute(
+                """
+                SELECT p.purchase_id, e.first_name, e.last_name, c.first_name, c.last_name, f.fuel_type, p.quantity, p.price, p.purchase_date
+                FROM fuel_purchases p
+                JOIN employees e ON p.employee_id = e.employee_id
+                JOIN customer_information c ON p.customer_id = c.customer_id
+                JOIN fuel_inventory f ON p.fuel_id = f.fuel_id
+                """
+            )
         return cursor.fetchall()
