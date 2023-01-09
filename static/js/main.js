b0a240848d80dbf9a8eb22609c2f63d0a4ffb506
Rijuth Menon <rijuthm@gmail.com>
2023-01-10 00:30:31 +0530

    added customer insights

--- static/js/main.js
+++ static/js/main.js
@@ -15,13 +15,27 @@ $(document).ready(function ()
     $('#employeeSelect').change(function ()
     {
         var employee = $(this).val();
-        $.getJSON('/employee_insights', { employee: employee }, function (data)
+        var data = $.param({ employee_id: employee });
+        $.getJSON('/employee_insights', data, function (data)
         {
-            console.log(data);
-
-            $('#totalFuelSales').text('₹' + data.total_sales_month);
+            $('#totalFuelSales').text(data.total_sales_month[0] ? '₹' + data.total_sales_month[0] : 0);
             $('#totalHoursWorked').text(data.total_hours_week[0] ? data.total_hours_week : 0);
             $('#avgHoursWorkedPerDay').text(data.avg_hours_day[0] ? data.avg_hours_day : 0);
+        });
+    }).change();
+});
+
+$(document).ready(function ()
+{
+    $('#customerSelect').change(function ()
+    {
+        var customer = $(this).val();
+        var data = $.param({ customer_id: customer });
+        $.getJSON('/customer_insights', data, function (data)
+        {
+            $('#totalMoneySpent').text(data.total_money_spent ? '₹' + data.total_money_spent : '₹' + 0);
+            $('#avgFuelPurchased').text(data.avg_fuel_purchased ? data.avg_fuel_purchased + ' litres' : '0 litres');
+            $('#frequentFuelType').text(data.frequent_fuel_type[0] ? data.frequent_fuel_type[0] : 'None');
         });
     }).change();
 });
