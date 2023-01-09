e71726ba0455670bfc4b620af664a9001fd0c002
Rijuth Menon <rijuthm@gmail.com>
2023-01-10 00:54:04 +0530

    added general insights

--- routes/insights.py
+++ routes/insights.py
@@ -11,9 +11,9 @@ def employee_insights():
     total_hours_week = Insights().total_hours_worked_week(employee_id)
     avg_hours_day = Insights().total_hours_worked_week(employee_id)
     return jsonify({
-        'total_sales_month': total_sales_month,
-        'total_hours_week': total_hours_week,
-        'avg_hours_day': avg_hours_day
+        'total_sales_month': total_sales_month[0],
+        'total_hours_week': total_hours_week[0],
+        'avg_hours_day': avg_hours_day[0]
     })
 
 
@@ -26,5 +26,19 @@ def customer_insights():
     return jsonify({
         'total_money_spent': total_money_spent,
         'avg_fuel_purchased': avg_fuel_purchased,
-        'frequent_fuel_type': frequent_fuel_type
+        'frequent_fuel_type': frequent_fuel_type[0]
+    })
+
+
+@insights_routes.route('/general_insights')
+def general_insights():
+    top_performing_employee = Insights().top_performing_employee()
+    popular_fuel_type = Insights().popular_fuel_type()
+    avg_purchase_quantity = Insights().avg_purchase_quantity()
+    total_monthly_sales = Insights().total_sales_month()
+    return jsonify({
+        'top_performing_employee': f'{top_performing_employee[0]} {top_performing_employee[1]}',
+        'popular_fuel_type': popular_fuel_type[0],
+        'avg_purchase_quantity': avg_purchase_quantity[0],
+        'total_monthly_sales': total_monthly_sales[0]
     })
