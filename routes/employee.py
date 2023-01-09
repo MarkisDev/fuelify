dad7ee2f9232ee7152c778b3ac4180eede51c71c
Rijuth Menon <rijuthm@gmail.com>
2023-01-10 02:22:23 +0530

    restructed routes for hours

--- routes/employee.py
+++ routes/employee.py
@@ -34,6 +34,30 @@ def add_employee():
     return redirect(url_for('employee_routes.employees'))
 
 
+@employee_routes.route('/delete_employee/<int:employee_id>', methods=['POST'])
+@login_required
+def delete_employee(employee_id):
+    Employee().delete(employee_id)
+    return redirect(url_for('employee_routes.employees'))
+
+
+@employee_routes.route('/update_employee', methods=['POST'])
+@login_required
+def update_employee():
+    employee_id = request.form['employee_id']
+    if request.form['first_name'] and request.form['last_name'] and request.form['phone'] and request.form['address'] and request.form['job_role'] and request.form['email'] and request.form['salary']:
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        phone = request.form['phone']
+        email = request.form['email']
+        job_role = request.form['job_role']
+        address = request.form['address']
+        salary = request.form['salary']
+        Employee().update(employee_id, first_name, last_name,
+                          phone, address, email, job_role, salary)
+    return redirect(url_for('employee_routes.employees'))
+
+
 @employee_routes.route('/add_hours', methods=['POST'])
 @login_required
 def add_hours():
@@ -64,28 +88,4 @@ def update_hours():
 @login_required
 def delete_hours(employee_hours_id):
     Employee().delete_employee_hours(employee_hours_id)
-    return redirect(url_for('employee_routes.employees'))
-
-
-@employee_routes.route('/delete_employee/<int:employee_id>', methods=['POST'])
-@login_required
-def delete_employee(employee_id):
-    Employee().delete(employee_id)
-    return redirect(url_for('employee_routes.employees'))
-
-
-@employee_routes.route('/update_employee', methods=['POST'])
-@login_required
-def update_employee():
-    employee_id = request.form['employee_id']
-    if request.form['first_name'] and request.form['last_name'] and request.form['phone'] and request.form['address'] and request.form['job_role'] and request.form['email'] and request.form['salary']:
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        phone = request.form['phone']
-        email = request.form['email']
-        job_role = request.form['job_role']
-        address = request.form['address']
-        salary = request.form['salary']
-        Employee().update(employee_id, first_name, last_name,
-                          phone, address, email, job_role, salary)
     return redirect(url_for('employee_routes.employees'))
