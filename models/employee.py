ba92c3c0d9b6abdc886b358f96f11e4679dc339a
Rijuth Menon <rijuthm@gmail.com>
2023-01-10 02:21:50 +0530

    added employee hours

--- models/employee.py
+++ models/employee.py
@@ -1,4 +1,3 @@
-import bcrypt
 from models.database import Database
 
 
@@ -57,5 +56,70 @@ class Employee(Database):
             """,
             (first_name, last_name, phone_number, address,
              email, job_role, salary, employee_id)
+        )
+        self.conn.commit()
+
+    def get_employee_hours(self, employee_id=None, employee_hours_id=None):
+        cursor = self.conn.cursor()
+        if employee_id:
+            cursor.execute(
+                """
+                SELECT e.first_name, e.last_name, h.*
+                FROM employee_hours h, employees e
+                WHERE employee_id = ?
+                """,
+                (employee_id,),
+            )
+            return cursor.fetchall()
+        elif employee_hours_id:
+            cursor.execute(
+                """
+                SELECT e.first_name, e.last_name, h.*
+                FROM employee_hours h, employees e
+                WHERE employee_hours_id = ?
+                """,
+                (employee_hours_id,),
+            )
+            return cursor.fetchone()
+        else:
+            cursor.execute(
+                """
+                SELECT e.first_name, e.last_name, h.*
+                FROM employee_hours h, employees e
+                """,
+            )
+            return cursor.fetchall()
+
+    def insert_employee_hours(self, employee_id, entry_date, hours_worked):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO employee_hours (employee_id, entry_date, hours_worked)
+            VALUES (?, ?, ?)
+            """,
+            (employee_id, entry_date, hours_worked),
+        )
+        self.conn.commit()
+
+    def delete_employee_hours(self, employee_hours_id):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            DELETE FROM employee_hours
+            WHERE employee_hours_id = ?
+            """,
+            (employee_hours_id,),
+        )
+        self.conn.commit()
+
+    def update_employee_hours(self, employee_hours_id, hours_worked, entry_date):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            UPDATE employee_hours
+            SET hours_worked = ?, entry_date =?
+            WHERE employee_hours_id = ?
+            """,
+            (hours_worked, entry_date, employee_hours_id),
         )
         self.conn.commit()
