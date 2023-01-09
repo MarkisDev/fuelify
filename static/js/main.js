e25c655af11ccdafaefd3a23037c1db470c095c6
Rijuth Menon <rijuthm@gmail.com>
2023-01-09 23:58:04 +0530

    added jquery code for employee insights

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,19 @@ $(document).ready(function ()
         border: "1"
     });
 });
+
+$(document).ready(function ()
+{
+    $('#employeeSelect').change(function ()
+    {
+        var employee = $(this).val();
+        $.getJSON('/employee_insights', { employee: employee }, function (data)
+        {
+            console.log(data);
+
+            $('#totalFuelSales').text('₹' + data.total_sales_month);
+            $('#totalHoursWorked').text(data.total_hours_week[0] ? data.total_hours_week : 0);
+            $('#avgHoursWorkedPerDay').text(data.avg_hours_day[0] ? data.avg_hours_day : 0);
+        });
+    }).change();
+});
