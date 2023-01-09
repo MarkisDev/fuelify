269c59f7400a0148503bb672d04828d27d900d7c
Rijuth Menon <rijuthm@gmail.com>
2023-01-10 00:53:48 +0530

    added general insights

--- static/js/main.js
+++ static/js/main.js
@@ -18,9 +18,9 @@ $(document).ready(function ()
         var data = $.param({ employee_id: employee });
         $.getJSON('/employee_insights', data, function (data)
         {
-            $('#totalFuelSales').text(data.total_sales_month[0] ? '₹' + data.total_sales_month[0] : 0);
-            $('#totalHoursWorked').text(data.total_hours_week[0] ? data.total_hours_week : 0);
-            $('#avgHoursWorkedPerDay').text(data.avg_hours_day[0] ? data.avg_hours_day : 0);
+            $('#totalFuelSales').text(data.total_sales_month ? '₹' + data.total_sales_month : '₹' + 0);
+            $('#totalHoursWorked').text(data.total_hours_week ? data.total_hours_week : 0);
+            $('#avgHoursWorkedPerDay').text(data.avg_hours_day ? data.avg_hours_day : 0);
         });
     }).change();
 });
@@ -35,7 +35,20 @@ $(document).ready(function ()
         {
             $('#totalMoneySpent').text(data.total_money_spent ? '₹' + data.total_money_spent : '₹' + 0);
             $('#avgFuelPurchased').text(data.avg_fuel_purchased ? data.avg_fuel_purchased + ' litres' : '0 litres');
-            $('#frequentFuelType').text(data.frequent_fuel_type[0] ? data.frequent_fuel_type[0] : 'None');
+            $('#frequentFuelType').text(data.frequent_fuel_type ? data.frequent_fuel_type[0] : 'None');
         });
     }).change();
+});
+
+$(document).ready(function ()
+{
+
+    $.getJSON('/general_insights', function (data)
+    {
+        $('#totalMonthlySales').text(data.total_monthly_sales ? '₹' + data.total_monthly_sales : '₹' + 0);
+        $('#avgPurchaseQuantity').text(data.avg_purchase_quantity ? data.avg_purchase_quantity + ' litres' : '0 litres');
+        $('#popularFuelType').text(data.popular_fuel_type ? data.popular_fuel_type : 'None');
+        $('#topPerformingEmployee').text(data.top_performing_employee ? data.top_performing_employee : 'None');
+
+    });
 });
